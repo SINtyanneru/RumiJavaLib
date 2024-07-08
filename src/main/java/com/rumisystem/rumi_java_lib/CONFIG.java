ed01ac53887b66aa9646259d168d5b51bd45ca31
SINtyanneru <irjitgd@gmail.com>
2024-07-08 11:52:19 +0900

    設定ファイル読み込み

---
+++ src/main/java/com/rumisystem/rumi_java_lib/CONFIG.java
@@ -0,0 +1,50 @@
+package com.rumisystem.rumi_java_lib;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CONFIG {
+	public ArrayNode DATA = new ArrayNode();
+
+	//設定ファイルの中身をかいせきする
+	public CONFIG() throws IOException {
+		String[] CONFIG_CONTENTS = new FILER(new File("./Config.ini")).OPEN_STRING().split("\n");
+
+		String SEKSHON = "";
+
+		for(String S:CONFIG_CONTENTS){
+			//セクションか否か
+			if(S.startsWith("[") && S.endsWith("]")){
+				SEKSHON = S.replace("[", "").replace("]", "");
+				DATA.setDATA(SEKSHON, new ArrayNode());
+			} else {
+				//値
+				String KEY = S.split("=")[0];
+				String VAL = S.split("=")[1];
+
+				if(VAL.startsWith("\"") && VAL.endsWith("\"")){
+					StringBuilder TEXT = new StringBuilder();
+
+					String[] SPLITERO = VAL.split("");
+					for(int I = 1; I < SPLITERO.length; I++){
+						String C = SPLITERO[I];
+						//ダブルコートが来たら死ぬ
+						if(C.equals("\"")){
+							//その前の文字がバックスラッシュなら、虫する
+							if(!SPLITERO[I - 1].equals("\\")){
+								break;
+							}
+						}
+
+						TEXT.append(C);
+					}
+
+					DATA.get(SEKSHON).setDATA(KEY, TEXT.toString());
+				} else {
+					DATA.get(SEKSHON).setDATA(KEY, Integer.parseInt(VAL));
+				}
+			}
+		}
+	}
+}
