d13943706eb6da6c584a54998e2afe49d20a574a
SINtyanneru <irjitgd@gmail.com>
2024-10-07 17:50:53 +0900

    hoi!

--- src/main/java/com/rumisystem/rumi_java_lib/Main.java
+++ src/main/java/com/rumisystem/rumi_java_lib/Main.java
@@ -31,55 +31,6 @@ import java.util.Base64;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			SSHServer SSHD = new SSHServer(4545, "/etc/ssh/ssh_host_rsa_key");
-			SSHD.SET_EVENT_LISTENER(new Connect_EVENT_LISTENER() {
-				@Override
-				public void CONNECT(CONNECT_EVENT SESSION) {
-					try {
-						StringBuilder LINE = new StringBuilder();
-						System.out.println("新しい接続");
-
-						//イベント登録
-						SESSION.SET_EVENT_LISTENER(new EVENT_LISTENER() {
-							@Override
-							public void Send(SendEvent E) {
-								try {
-									if (!(E.getTEXT().equals("\r") || E.getTEXT().equals("\n"))) {
-										LINE.append(E.getTEXT());
-										SESSION.Send(E.getTEXT());
-									} else {
-										SESSION.Send("あなたは「" + LINE + "」を選択しました！\r\n");
-										LINE.delete(0, LINE.length());
-									}
-								} catch (Exception EX) {
-									EX.printStackTrace();
-								}
-							}
-
-							@Override
-							public void Close() {
-								System.out.println("切断された");
-							}
-						});
-
-						SESSION.Send("るみSSHへようこそ\r\n");
-						SESSION.Send("何をしますか？\r\n");
-						SESSION.Send("\r\n");
-						SESSION.Send("1：合同会社るみしすてむへのお問い合わせ\r\n");
-						SESSION.Send("2：るみさーばーの著作権侵害申立\r\n");
-						SESSION.Send("3：その他\r\n");
-						SESSION.Send("\r\n");
-						SESSION.Send("※数字で選択してください\r\n");
-
-					} catch (Exception EX) {
-						EX.printStackTrace();
-					}
-				}
-			});
-
-
-			SSHD.START();
-
 			/*
 			MisskeyClient MC = new MisskeyClient("ussr.rumiserver.com");
 			if (MC.TOKEN_LOGIN("4LfqJNO9w8x1x6rD") == LOGIN_RESULT.DONE) {
