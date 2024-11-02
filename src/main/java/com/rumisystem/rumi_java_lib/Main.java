6be6e9acb9ca1cc0d9c612b4b4280f189ccd6ace
SINtyanneru <irjitgd@gmail.com>
2024-11-02 21:59:20 +0900

    RID

--- src/main/java/com/rumisystem/rumi_java_lib/Main.java
+++ src/main/java/com/rumisystem/rumi_java_lib/Main.java
@@ -10,45 +10,7 @@ import com.rumisystem.rumi_java_lib.Socket.Server.SocketServer;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					SocketServer SS = new SocketServer();
-
-					SS.setEventListener(new CONNECT_EVENT_LISTENER() {
-						@Override
-						public void CONNECT(CONNECT_EVENT SESSION) {
-							try {
-								System.out.println("New SESSION 81:" + SESSION.getIP());
-								SESSION.sendMessage("a\n");
-
-								SESSION.setEventListener(new EVENT_LISTENER() {
-									@Override
-									public void Message(MessageEvent E) {
-										System.out.println("受信：" + E.getString());
-										if (E.getString().equals("QUIT")) {
-											SESSION.close();
-										}
-									}
-
-									@Override
-									public void Close(CloseEvent E) {
-										System.out.println("切断");
-									}
-								});
-							} catch (Exception EX) {
-								EX.printStackTrace();
-							}
-						}
-					});
-
-					try {
-						SS.START(8081);
-					} catch (Exception EX) {
-						EX.printStackTrace();
-					}
-				}
-			}).start();
+			System.out.println(RID.GEN());
 
 			/*
 			MisskeyClient MC = new MisskeyClient("ussr.rumiserver.com");
