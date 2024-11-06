80c7cc0b024e141b542f546c4f539971809b2150
SINtyanneru <irjitgd@gmail.com>
2024-11-07 03:59:59 +0900

    スノフレ

--- src/main/java/com/rumisystem/rumi_java_lib/Main.java
+++ src/main/java/com/rumisystem/rumi_java_lib/Main.java
@@ -15,6 +15,11 @@ import com.rumisystem.rumi_java_lib.WebSocket.Client.WebSocketClient;
 public class Main {
 	public static void main(String[] args) {
 		try {
+			while (true) {
+				System.out.println(SnowFlake.GEN());
+				Thread.sleep(1000);
+			}
+			/*
 			MisskeyClient MC = new MisskeyClient("ussr.rumiserver.com");
 			if (MC.TOKEN_LOGIN(args[0]) == LOGIN_RESULT.DONE) {
 				MC.SET_EVENT_LISTENER(new EVENT_LISTENER() {
@@ -48,7 +53,7 @@ public class Main {
 				});
 			} else {
 				System.out.println("ログイン失敗");
-			}
+			}*/
 		} catch (Exception EX) {
 			EX.printStackTrace();
 		}
