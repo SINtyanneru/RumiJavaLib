88aa2f15d97e665da32ea8ad03380013148eeb6f
SINtyanneru <irjitgd@gmail.com>
2024-11-12 02:27:01 +0900

    replaceでもいいらしい

--- src/main/java/com/rumisystem/rumi_java_lib/SANITIZE.java
+++ src/main/java/com/rumisystem/rumi_java_lib/SANITIZE.java
@@ -2,41 +2,41 @@ package com.rumisystem.rumi_java_lib;
 
 public class SANITIZE {
 	public static String CONSOLE_SANITIZE(String TEXT) {
-		TEXT = TEXT.replaceAll("\u0000", "[NULL]");
-		TEXT = TEXT.replaceAll("\u0001", "[SOH]");
-		TEXT = TEXT.replaceAll("\u0002", "[STX]");
-		TEXT = TEXT.replaceAll("\u0003", "[ETX]");
-		TEXT = TEXT.replaceAll("\u0004", "[EOT]");
-		TEXT = TEXT.replaceAll("\u0005", "[ENQ]");
-		TEXT = TEXT.replaceAll("\u0006", "[ACK]");
-		TEXT = TEXT.replaceAll("\u0007", "[BEL]");
-		TEXT = TEXT.replaceAll("\u0008", "[BS]");
-		TEXT = TEXT.replaceAll("\u0009", "[HT]");
-		//TEXT = TEXT.replaceAll("\n", "[LF]");
-		TEXT = TEXT.replaceAll("\u000b", "[VT]");
-		TEXT = TEXT.replaceAll("\u000c", "[FF]");
-		//TEXT = TEXT.replaceAll("\r", "[CR]");
-		TEXT = TEXT.replaceAll("\u000e", "[SO]");
-		TEXT = TEXT.replaceAll("\u000f", "[SI]");
-		TEXT = TEXT.replaceAll("\u0010", "[DLE]");
-		TEXT = TEXT.replaceAll("\u0011", "[DC1]");
-		TEXT = TEXT.replaceAll("\u0012", "[DC2]");
-		TEXT = TEXT.replaceAll("\u0013", "[DC3]");
-		TEXT = TEXT.replaceAll("\u0014", "[DC4]");
-		TEXT = TEXT.replaceAll("\u0015", "[NAK]");
-		TEXT = TEXT.replaceAll("\u0016", "[SYN]");
-		TEXT = TEXT.replaceAll("\u0017", "[ETB]");
-		TEXT = TEXT.replaceAll("\u0018", "[CAN]");
-		TEXT = TEXT.replaceAll("\u0019", "[EM]");
-		TEXT = TEXT.replaceAll("\u001a", "[SUB]");
-		TEXT = TEXT.replaceAll("\u001b", "[EXT/ANSI]");
-		TEXT = TEXT.replaceAll("\u001c", "[FS]");
-		TEXT = TEXT.replaceAll("\u001d", "[GS]");
-		TEXT = TEXT.replaceAll("\u001e", "[RS]");
-		TEXT = TEXT.replaceAll("\u001f", "[US]");
-		TEXT = TEXT.replaceAll("\u007f", "[DEL]");
-
+		TEXT = TEXT.replace("\u0000", "[NULL]");
+		TEXT = TEXT.replace("\u0001", "[SOH]");
+		TEXT = TEXT.replace("\u0002", "[STX]");
+		TEXT = TEXT.replace("\u0003", "[ETX]");
+		TEXT = TEXT.replace("\u0004", "[EOT]");
+		TEXT = TEXT.replace("\u0005", "[ENQ]");
+		TEXT = TEXT.replace("\u0006", "[ACK]");
+		TEXT = TEXT.replace("\u0007", "[BEL]");
+		TEXT = TEXT.replace("\u0008", "[BS]");
+		TEXT = TEXT.replace("\u0009", "[HT]");
+		//TEXT = TEXT.replace("\n", "[LF]");
+		TEXT = TEXT.replace("\u000b", "[VT]");
+		TEXT = TEXT.replace("\u000c", "[FF]");
+		//TEXT = TEXT.replace("\r", "[CR]");
+		TEXT = TEXT.replace("\u000e", "[SO]");
+		TEXT = TEXT.replace("\u000f", "[SI]");
+		TEXT = TEXT.replace("\u0010", "[DLE]");
+		TEXT = TEXT.replace("\u0011", "[DC1]");
+		TEXT = TEXT.replace("\u0012", "[DC2]");
+		TEXT = TEXT.replace("\u0013", "[DC3]");
+		TEXT = TEXT.replace("\u0014", "[DC4]");
+		TEXT = TEXT.replace("\u0015", "[NAK]");
+		TEXT = TEXT.replace("\u0016", "[SYN]");
+		TEXT = TEXT.replace("\u0017", "[ETB]");
+		TEXT = TEXT.replace("\u0018", "[CAN]");
+		TEXT = TEXT.replace("\u0019", "[EM]");
+		TEXT = TEXT.replace("\u001a", "[SUB]");
+		TEXT = TEXT.replace("\u001b", "[EXT/ANSI]");
+		TEXT = TEXT.replace("\u001c", "[FS]");
+		TEXT = TEXT.replace("\u001d", "[GS]");
+		TEXT = TEXT.replace("\u001e", "[RS]");
+		TEXT = TEXT.replace("\u001f", "[US]");
+		TEXT = TEXT.replace("\u007f", "[DEL]");
 
 		return TEXT;
 	}
+
 }
