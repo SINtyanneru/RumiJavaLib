7992b3ac7dc8afa964600706db6a8b130ea96d09
SINtyanneru <irjitgd@gmail.com>
2025-07-02 07:22:29 +0900

    トークン投げないと取得できなくなってる

--- src/main/java/su/rumishistem/rumi_java_lib/Misskey/API/UserSHOW.java
+++ src/main/java/su/rumishistem/rumi_java_lib/Misskey/API/UserSHOW.java
@@ -13,18 +13,26 @@ public class UserSHOW {
 	public static User GetUID(String DOMAIN, String UID, User Kai, String TOKEN) throws IOException {
 		FETCH AJAX = new FETCH("https://" + DOMAIN + "/api/users/show");
 		AJAX.SetHEADER("Content-Type", "application/json; charset=utf-8");
-		FETCH_RESULT RESULT = AJAX.POST(("{\"username\":\"" + UID + "\"}").getBytes());
+		FETCH_RESULT RESULT = AJAX.POST(("{\"username\":\"" + UID + "\", \"i\": \""+TOKEN+"\"}").getBytes());
 		JsonNode UserData = new ObjectMapper().readTree(RESULT.GetRAW());
 
-		return ConvertType.ConvertUser(UserData, Kai, DOMAIN, TOKEN);
+		if (RESULT.GetSTATUS_CODE() == 200) {
+			return ConvertType.ConvertUser(UserData, Kai, DOMAIN, TOKEN);
+		} else {
+			throw new Error("APIエラー:" + UserData.get("error").get("message").asText());
+		}
 	}
 
 	public static User GetID(String DOMAIN, String ID, User Kai, String TOKEN) throws IOException {
 		FETCH AJAX = new FETCH("https://" + DOMAIN + "/api/users/show");
 		AJAX.SetHEADER("Content-Type", "application/json; charset=utf-8");
-		FETCH_RESULT RESULT = AJAX.POST(("{\"userId\":\"" + ID + "\"}").getBytes());
+		FETCH_RESULT RESULT = AJAX.POST(("{\"userId\":\"" + ID + "\", \"i\":\""+TOKEN+"\"}").getBytes());
 		JsonNode UserData = new ObjectMapper().readTree(RESULT.GetRAW());
 
-		return ConvertType.ConvertUser(UserData, Kai, DOMAIN, TOKEN);
+		if (RESULT.GetSTATUS_CODE() == 200) {
+			return ConvertType.ConvertUser(UserData, Kai, DOMAIN, TOKEN);
+		} else {
+			throw new Error("APIエラー:" + UserData.get("error").get("message").asText());
+		}
 	}
 }
