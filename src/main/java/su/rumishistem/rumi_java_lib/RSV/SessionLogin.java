43decf62698d2b1f59826e7d2066bb906267584e
SINtyanneru <irjitgd@gmail.com>
2026-01-27 00:07:44 +0900

    un

--- src/main/java/su/rumishistem/rumi_java_lib/RSV/SessionLogin.java
+++ src/main/java/su/rumishistem/rumi_java_lib/RSV/SessionLogin.java
@@ -2,8 +2,11 @@ package su.rumishistem.rumi_java_lib.RSV;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import su.rumishistem.rsdf_java.RSDFDecoder;
 import su.rumishistem.rumi_java_lib.FETCH;
 import su.rumishistem.rumi_java_lib.FETCH_RESULT;
+
+import java.util.Map;
 
 public class SessionLogin {
 	private boolean login = false;
@@ -12,14 +15,17 @@ public class SessionLogin {
 
 	public SessionLogin(String host, String session) {
 		try {
-			FETCH ajax = new FETCH("http://" + host + "/Session?ID=" + session + "&SERVICE=STORAGE");
+			FETCH ajax = new FETCH("http://" + host + "/api/Session?ID=" + session);
 			FETCH_RESULT result = ajax.GET();
 			if (result.getStatusCode() == 200) {
-				JsonNode RESULT = new ObjectMapper().readTree(result.getString());
-				if (RESULT.get("STATUS").asBoolean()) {
+				Map<String, Object> d = RSDFDecoder.decode(result.getRaw()).get_dict();
+
+				if ((Boolean)d.get("STATUS")) {
 					this.login = true;
 					this.session = session;
-					this.account_data = RESULT.get("ACCOUNT_DATA");
+
+					JsonNode json = new ObjectMapper().readTree(new ObjectMapper().writeValueAsString(d.get("ACCOUNT_DATA")));
+					this.account_data = json;
 				}
 			}
 		} catch (Exception EX) {
